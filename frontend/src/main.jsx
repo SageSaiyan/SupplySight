a32c0795d5646ce0331e9d145dba9e473794250b
SageSaiyan <162331004+SageSaiyan@users.noreply.github.com>
2025-08-09 22:53:17 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,18 @@ import App from './App.jsx'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
-import markerIcon from 'leaflet/dist/images/marker-icon.png'
-import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
+const iconBase = 'https://unpkg.com/leaflet@1.9.4/dist/images/'
+
+L.Marker.prototype.options.icon = L.icon({
+  iconRetinaUrl: iconBase + 'marker-icon-2x.png',
+  iconUrl: iconBase + 'marker-icon.png',
+  shadowUrl: iconBase + 'marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
 })
 import { AuthProvider } from './contexts/AuthContext'
 import { NotificationProvider } from './contexts/NotificationContext'
